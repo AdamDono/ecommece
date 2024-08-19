116ed5ea72adc00651ef62c173657e5eb74012f1
AdamDono <>
2024-08-19 02:58:53 +0200

    get method on constructor

--- src/app/serices/api.service.ts
+++ src/app/serices/api.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
-}
+  constructor(
+    private HttpClient: HttpClient
+  ) { }
+  
+    get<T>(url: string, options?: any): Observable <T> {return this.HttpClient.get<T>(url, options) as Observable<T>;}
+  }
+
